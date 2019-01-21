3bc56d6eac7bfc1268f3bc51b5a806c002dc582b
Ravenhost <ashtazar@gmail.com>
2019-01-21 14:28:09 +0700

    Fix internal middleware Expand routeNotFound logging

--- responses/routeNotFound.js
+++ responses/routeNotFound.js
@@ -1,5 +1,7 @@
 module.exports = (req, res, next) => {
   console.log('Attempt to access nonexistent or internal route without proper authorization.');
+  console.log(`Address requested: ${req.originalUrl}`);
+  console.log(`Headers: ${JSON.stringify(req.headers)}`);
 
   return res.status(404).json({ message: `Route you're trying to access doesn't exist.` });
 };
